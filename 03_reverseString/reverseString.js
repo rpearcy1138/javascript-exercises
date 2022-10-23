561543039a0e1b3a6e1e38f2306f54ac62c64aac
Ray Pearcy <rrpearcy@icloud.com>
2022-10-23 12:37:05 -0500

    solved reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let string = word;
+    let splitString = string.split('');
+    let reverseArray = splitString.reverse();
+    let reverseString = reverseArray.join('');
+    return reverseString;
 };
 
 // Do not edit below this line
